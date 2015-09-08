10a801d6761c7752a6f82765ca52ea44df1d8401
AnvarTurobov <anvar.turobov@gmail.com>
2015-09-08 11:40:09 +0100

    created tutorials collection to store and orgonize the date

---
+++ tutorials/app/lib/collections/tutorials.js
@@ -0,0 +1,32 @@
+Tutorials = new Mongo.Collection('tutorials');
+
+
+if (Meteor.isServer) {
+  Tutorials.allow({
+    insert: function (userId, doc) {
+      return false;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return false;
+    },
+
+    remove: function (userId, doc) {
+      return false;
+    }
+  });
+
+  Tutorials.deny({
+    insert: function (userId, doc) {
+      return true;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return true;
+    },
+
+    remove: function (userId, doc) {
+      return true;
+    }
+  });
+}
